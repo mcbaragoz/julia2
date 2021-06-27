55e9e2aa486a0d9b142550fc11b49545db0111c5
mcbaragoz <semenabramov2002@gmail.com>
2021-06-27 13:38:18 +0300

    практика 15

---
+++ practice15/task1.jl
@@ -0,0 +1,20 @@
+﻿ConnectList{T}=Vector{Vector{T}}
+NestedVectors = Vector
+
+function convert_to_nested(tree::ConnectList{T},root::T) where T
+    nested_tree = []
+    for subroot in tree[root]
+        push!(nested_tree, convert(tree, subroot))
+    end
+    push!(nested_tree, root)
+    return nested_tree
+end
+
+tree=[[2,3],
+      [],
+      [4,5],
+      [],
+      []]
+
+nested_tree = convert_to_nested(tree, 1) 
+println(nested_tree)
---
+++ practice15/task2.jl
@@ -0,0 +1,23 @@
+﻿function convert_to_dict(tree::NestedVectors)
+    T=typeof(tree[end])
+    connect_tree = Dict{T,Vector{T}}()
+    
+    function recurs_trace(tree)
+        connect_tree[tree[end]]=[]
+        for subtree in tree[1:end-1] # - перебор всех поддеревьев
+            push!(connect_tree[tree[end]], recurs_trace(subtree))
+        end
+        return tree[end] # - индекс конрня
+    end
+
+    recurs_trace(tree)
+    return connect_tree
+end
+
+function convert_to_list(tree::Dict{T,Vector{T}}) where T
+    list_tree=Vector{Vector{T}}(undef,length(tree))
+    for subroot in eachindex(list_tree)
+        list_tree[subroot]=tree[subroot]
+    end
+    return list_tree
+end
