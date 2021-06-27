345e1fe918087a33b8af05e692e727d535a5e5b1
mcbaragoz <semenabramov2002@gmail.com>
2021-06-27 13:38:46 +0300

    практика 17

---
+++ practice17./task1.jl
@@ -0,0 +1,17 @@
+﻿function dfsearch(startver::T, graph::ConnectList{T}) where T
+    mark = zeros(Bool, length(graph))
+    stack  = [startver]
+    mark[startver] = 1
+    visited = Int64[]
+    while !isempty(stack)
+        v = pop!(stack)
+        push!(visited,v)
+        for u in graph[v]
+            if mark[u] == 0
+                push!(stack,u)
+                mark[u] = 1
+            end
+        end
+    end
+    return visited
+end
---
+++ practice17./task3.jl
@@ -0,0 +1,7 @@
+﻿function valence(graph::ConnectList{T}) where T
+    val = zeros(size(graph,1))
+    for i in 1:size(graph,1)
+        val[i]=length(graph[i])
+    end
+    return val
+end
